0840ccc5cd81ec55db95a8c5fdca074d6fc7f929
Juan Rezende <juanvictor.rezende11@gmail.com>
2022-07-22 21:48:29 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+</template>
+
+<script setup>
+import todoList from "./components/todoList.vue";
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import App from './components/todoList.vue'
+
+import './assets/main.css'
+
+createApp(App).mount('#app')
